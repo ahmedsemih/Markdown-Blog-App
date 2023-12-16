d447000b9ebf5432672fd26cdfb0768008aa8cef
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2023-12-16 08:02:46 +0300

    create post section and implement to category page

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,6 +1,6 @@
-import styles from "./CategoryPage.module.css";
-import { Card, FeatureSection } from "@/components";
-import { Post, allPosts } from "contentlayer/generated";
+import { PostSection } from "@/components";
+import { FeatureSection } from "@/components";
+import { allPosts } from "contentlayer/generated";
 
 type Props = {
   params: {
@@ -15,21 +15,7 @@ const CategoryPage = ({ params }: Props) => {
   return (
     <main className="container page">
       <FeatureSection posts={featuredPosts} category={params.category} />
-      <section className={styles.section}>
-        <h1 className={styles.title}>All Posts</h1>
-        <p className={styles.subTitle}>
-          All posts in the {params.category} category.
-        </p>
-        <div className={styles.posts}>
-          {posts?.length > 0 ? (
-            posts.map((post: Post) => <Card key={post._id} post={post} />)
-          ) : (
-            <p className={styles.noPost}>
-              There are no posts under this category.
-            </p>
-          )}
-        </div>
-      </section>
+      <PostSection posts={posts} category={params.category} />
     </main>
   );
 };
---
+++ src/components/PostSection/index.tsx
@@ -0,0 +1,32 @@
+import { Card } from "..";
+import { Post } from "contentlayer/generated";
+import styles from "./PostSection.module.css";
+
+type Props = {
+  posts: Post[];
+  category?: string;
+};
+
+const PostSection = ({ posts, category }: Props) => {
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.title}>All Posts</h1>
+      <p className={styles.subTitle}>
+        {category
+          ? `All posts in the ${category} category.`
+          : `All posts from newest to oldest.`}
+      </p>
+      <div className={styles.posts}>
+        {posts?.length > 0 ? (
+          posts.map((post: Post) => <Card key={post._id} post={post} />)
+        ) : (
+          <p className={styles.noPost}>
+            There are no posts under this category.
+          </p>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default PostSection;
